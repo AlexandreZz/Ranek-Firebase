913198d3eb86a0652f8b7c74d6707f1e5df00453
alexandre <alexandre@DESKTOP-KICTIUG.localdomain>
2022-10-24 14:12:53 -0300

    terminei a parte do contato.

--- src/components/contato/Contato.js
+++ src/components/contato/Contato.js
@@ -1,7 +1,27 @@
 import React from "react";
+import styles from "../../modules/Contato.module.css";
+import datilografia from "../img/datilografia.jpg";
 
 const Contato = () => {
-  return <div>Contato</div>;
+  return (
+    <section className={`${styles.contato} flex animacaoLEFT center`}>
+      <div className={styles.image}>
+        <img src={datilografia} alt="DATILOGRAFIA" title="DATILOGRAFIA" />
+      </div>
+      <div className={styles.descricao}>
+        <h2>Entre em contato.</h2>
+        <p>
+          <span />AlexandreZz@email.com
+        </p>
+        <p>
+          <span />9999-9999
+        </p>
+        <p>
+          <span />Rua Ali Perto,999
+        </p>
+      </div>
+    </section>
+  );
 };
 
 export default Contato;
