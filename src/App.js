c645aa4fd7e15e805b3dcf6cf1262af4ee298af8
alexandre <alexandre@DESKTOP-KICTIUG.localdomain>
2022-10-28 11:20:25 -0300

    Adicionei o BrowserRouter ao nosso index, ocorreu um erro ao usar o navigator, então precisei adicionar no index o BrowserRoute em todo o nosso aplicativo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Produtos from "./components/Produtos";
@@ -9,22 +9,44 @@ import NotFound from "./components/NotFound";
 import Home from "./components/Home";
 import "./App.css";
 import Preloading from "./components/Preloading";
+import Main from "./components/Main";
+import "./components/InitialFireBase";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+const auth = getAuth();
 
 function App() {
+  const [logado, setLogado] = React.useState();
+  const navigator = useNavigate();
+
+  React.useEffect(() => {
+    onAuthStateChanged(auth, user => {
+      if (user) {
+        // const uid = user.uid;
+        setLogado(true);
+        navigator("/main");
+      } else {
+        // User is signed out
+        setLogado(false);
+        navigator("/login");
+      }
+    });
+  }, []);
+
   return (
     <section>
-      <BrowserRouter>
-        <Header />
-        {/* <Preloading /> */}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="produtos/:id" element={<Produtos />} />
-          <Route path="contato" element={<Contato />} />
-          <Route path="login/*" element={<Login />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
+      <Header />
+      <Preloading />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="produtos/:id" element={<Produtos />} />
+        <Route path="contato" element={<Contato />} />
+        {logado === true
+          ? <Route path="main/*" element={<Main />} />
+          : <Route path="login/*" element={<Login />} />}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
     </section>
   );
 }
